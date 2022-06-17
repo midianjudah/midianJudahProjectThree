e79c1eaad5e5b2ca9e810c21f4d789f6d49cb44b
Midian Judah <midianjudah@gmail.com>
2022-06-17 18:36:04 -0400

    added header, imports, api key

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
+// Impot AXIOS package
+import axios from 'axios';
+// Import useEffect & useState Hook
+import { useEffect, useState } from 'react';
+// Import header
+import Header from './Header';
+// Import form
 
 function App() {
+  // console.log('App has rendered');
+
+  // useState FIRST
+  const [ randomArtist, setRandomArtist ] = useState([]);
+
+  // Here we impliment AXIOS
+  useEffect( () => {
+    axios({
+      url: `http://ws.audioscrobbler.com/2.0/`,
+      method: `GET`,
+      dataResponse: `json`,
+      params: {
+        key: `f26c9d8874c4331577c88575e4046583`,
+        format: `json`,
+      }
+      .then ( jsonData => {
+        console.log(jsonData);
+      })
+    })
+  })
+
+
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>Music Mirror</h1>
+            <h2>Music Mirror on the wall, who's the most talented of them all?</h2>
+        </header>
+    )
+}
+
+export default Header;
