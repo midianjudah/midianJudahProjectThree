6418246541d01064ebc0a98161cfc0273799a11b
Midian Judah <midianjudah@gmail.com>
2022-06-21 15:40:41 -0400

    got list to display to the page

--- src/App.js
+++ src/App.js
@@ -12,32 +12,36 @@ import Footer from './Footer.js';
 import { render } from '@testing-library/react';
 
 function App() {
-   console.log('App has rendered');
+  console.log('App has rendered');
 
   //useState FIRST
   //const [ randomArtist, setRandomArtist ] = useState([]);
 
   const [selectArtist, setSelectArtist] = useState('');
+  const [artistData, setArtistData] = useState([]);
 
   //Here we impliment AXIOS and the useEffect
-  useEffect( () => {
-    axios({
-      url: `http://ws.audioscrobbler.com/2.0/`,
-      method: `GET`,
-      dataResponse: `json`,
-      params: {
-        api_key: `f26c9d8874c4331577c88575e4046583`,
-        artist: selectArtist,
-        format: `json`,
-        method: `artist.getSimilar`,
-        limit: 10,
-      }
-    }).then ( jsonData => {
-        console.log(jsonData);
-    })
-  },[selectArtist]);
+  useEffect(() => {
+    if (selectArtist != '') {
+      axios({
+        url: `http://ws.audioscrobbler.com/2.0/`,
+        method: `GET`,
+        dataResponse: `json`,
+        params: {
+          api_key: `f26c9d8874c4331577c88575e4046583`,
+          artist: selectArtist,
+          format: `json`,
+          method: `artist.getSimilar`,
+          limit: 10,
+        }
+      }).then(jsonData => {
+        setArtistData(jsonData.data.similarartists.artist);
+        console.log(jsonData.data.similarartists.artist)
+      })
+    }
+  }, [selectArtist]);
 
-  const selectArtistSelection=function(event, artist) {
+  const selectArtistSelection = function (event, artist) {
 
     event.preventDefault();
     setSelectArtist(artist)
@@ -46,11 +50,11 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Form handleSubmit={selectArtistSelection} selectArtist={selectArtist}/>
+      <Form handleSubmit={selectArtistSelection} selectArtist={selectArtist} artistData={artistData} />
       <Footer />
     </div>
   );
-  }
+}
 
 
 export default App;
--- src/Form.js
+++ src/Form.js
@@ -2,24 +2,18 @@ import { useState } from 'react';
 
 const Form = (props) => {
 
-
     const [selectValue, setSelectValue] = useState('');
-
 
     const handleChange = (event) => {
         setSelectValue(event.target.value);
+        
     }
 
     const submitHandler = (event) => {
         event.preventDefault();
         props.handleSubmit(event, selectValue);
-        console.log(props.selectArtist)
-
-
+        
     }
-
-
-
 
     return (
         <>
@@ -27,24 +21,42 @@ const Form = (props) => {
                 onSubmit={submitHandler}
             >
                 <label htmlFor="artistSelection">Please Select and Artist:</label>
-                <select name="" id="artistSelection" onChange={handleChange} value={props.selectValue}
+                <select name="" id="artistSelection" defaultValue={'placeholder'} onChange={handleChange}
                 >
-                    <option value="placeholder" disabled selected>Select an artist</option>
-                    <option value="drake">Drake</option>
-                    <option value="cream">Cream</option>
-
+                    <option value="placeholder" disabled>Select an artist</option>
+                    <option value="B. B. King">B. B. King</option>
+                    <option value="Chuck Berry">Chuck Berry</option>
+                    <option value="Cream">Cream</option>
+                    <option value="Pink Floyd">Pink Floyd</option>
+                    <option value="Muddy Waters">Muddy Waters</option>
+                    <option value="Drake">Drake</option>
+                    <option value="Kanye West">Kanye West</option>
+                    <option value="nas">Nas</option>
+                    <option value="Merle Haggard">Merle Haggard</option>
+                    <option value="kraftwerk">KraftWerk</option>
+                    <option value="bob dylan">Bob Dylan</option>
+                    <option value="miles davis">Miles Davis</option>
+                    <option value="Ella Fitzgerald">Ella Fitzgerald</option>
+                    <option value="Madonna">Madonna</option>
+                    <option value="Michael Jackson">Michael Jackson</option>
+                    <option value="Stevie Wonder">Stevie Wonder</option>
+                    <option value="Bob Marley">Bob Marley</option>
+                    <option value="Netsky">Netsky</option>
+                    <option value="Bladerunner">Bladerunner</option>
+                    <option value="Deadmou5">Deadmau5</option>
                 </select>
 
                 <button>Submit</button>
             </form>
             <>
-                
                 <>
-                    <p>{props.selectArtist}</p>
-                    {/* 
-                    I'm having trouble targeting the similar artists and I'm having trouble getting down into the levels I need it to go.
-                    Don't know what path I need to go!
-                     */}
+                {
+                    props.artistData.map((artist)=>{
+                        return(
+                            <p key={artist.mbid}>{artist.name}</p>
+                        )
+                    })
+                }
                 </>
                 
             </>
